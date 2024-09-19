f48cf950f074e9a254dc541fd5e243dbd87ff1a3
lbonsenor <lbonsenor@itba.edu.ar>
2024-09-18 23:21:19 -0300

    Color palette added for testing

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,9 +11,35 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+const lightTheme = {
+  dark: false,
+  colors: {
+    primary: '#5D24FF',
+    onPrimary: '#FFFFFF',
+    error: '#BA1A1A',
+    onError: '#FFFFFF',
+    success: '#009217',
+    background: 'FDF8FF',
+  }
+}
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    primary: '#5D24FF',
+    onPrimary: '#FFFFFF',
+    error: '#BA1A1A',
+    onError: '#FFFFFF',
+    success: '#009217',
+    background: '121212',
+  }
+
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'darkTheme',
+    themes: {lightTheme, darkTheme}
   },
 })
