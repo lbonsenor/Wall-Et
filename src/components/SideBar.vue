a3f17106c758cdd73e7f5e13f2b08443773b1e71
lbonsenor <lbonsenor@itba.edu.ar>
2024-10-02 15:09:04 -0300

    small changes
    Co-authored-by: clee0412 <clee0412@users.noreply.github.com>

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,24 +1,40 @@
 <template>
-    <v-navigation-drawer permanent color="surface" expand-on-hover rail style="height: 92vh; margin-top: 4vh;" class="rounded-e-xl py-6 px-2">
-      <v-list base-color="primary">
-        <v-list-item rounded="xl" nav slim prepend-icon="mdi-home-outline" title="Inicio" value="home"/>
-        <v-list-item rounded="xl" nav slim prepend-icon="mdi-send-outline" title="Transferir Dinero" value="transfer"/>
-        <v-list-item rounded="xl" nav slim prepend-icon="mdi-receipt-text-clock-outline" title="Actividades" value="activities"/>
-        <v-list-item rounded="xl" nav slim prepend-icon="mdi-credit-card-outline" title="Mis Tarjetas" value="cards"/>
-        <v-list-item rounded="xl" nav slim prepend-icon="mdi-piggy-bank-outline" title="Cobrar" value="charge"/>
-      </v-list>
+  <v-navigation-drawer permanent color="surface" expand-on-hover rail style="height: 92vh; margin-top: 4vh;" class="rounded-e-xl py-6 px-2">
+    <v-list base-color="primary">
+      <v-list-item 
+        v-for="(item, i) in items" 
+        nav rounded="xl"
+        :key="i"
+        :value="item.value"
+        :title="item.title"
+        :prepend-icon="item.icon"
+        variant="plain"
+        >
 
-      <template v-slot:append>
-        <v-list>
-          <v-list-item nav rounded="xl" prepend-icon="mdi-cog-outline" title="Configuración" value="settings" base-color="primary"/>
-          <v-list-item nav rounded="xl" prepend-icon="mdi-logout" title="Cerrar Sesión" value="logout" base-color="error" />
-        </v-list>
-      </template>
-    </v-navigation-drawer>
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <v-list>
+        <v-list-item nav rounded="xl" prepend-icon="mdi-cog-outline" title="Configuración" value="settings" base-color="primary"/>
+        <v-list-item nav rounded="xl" prepend-icon="mdi-logout" title="Cerrar Sesión" value="logout" base-color="error" />
+      </v-list>
+    </template>
+  </v-navigation-drawer>
 </template>
   
-<script setup>
-
+<script>
+  export default{
+    data: () => ({
+      items: [
+        {title: 'Inicio', value:"home", icon:"mdi-home-outline"},
+        {title: 'Transferir Dinero', value:"transfer", icon:"mdi-send-outline"},
+        {title: 'Actividades', value:"activities", icon:"mdi-receipt-text-clock-outline"},
+        {title: 'Mis Tarjetas', value:"cards", icon:"mdi-credit-card-outline"},
+        {title: 'Cobrar', value:"charge", icon:"mdi-piggy-bank-outline"},
+      ]
+    })
+  }
   
 </script>
 
