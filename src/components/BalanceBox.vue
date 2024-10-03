96cc6b9864a2fdb40fc721f6a735a9fbe0cf9492
Camila <clee@itba.edu.ar>
2024-10-02 23:52:55 -0300

    balance box finished
    Co-authored-by: Lautaro Bonseñor <lbonsenor@users.noreply.github.com>

--- src/components/BalanceBox.vue
+++ src/components/BalanceBox.vue
@@ -1,16 +1,18 @@
 <template>
     <v-card class="mx-auto px-5 py-4 rounded-xl" 
-        color="surface"
-        max-width="290"
-        >
-        <v-text style="font-size: medium; font-weight: 500; color:rgb(var(--v-theme-title))" color="error">Dinero Disponible</v-text>
+            color="surface"
+            min-width="280"
+            max-width="50vw">
+        <v-text style="font-weight: 500; color:rgb(var(--v-theme-title))">Dinero Disponible</v-text>
         <div class="d-flex container">
-            <v-text class="me-auto" style="font-size: xx-large; font-weight: bold">
-                {{ balanceString }}
+            <v-text class="me-auto text-no-wrap balance">
+                {{ isBalanceShown ? formatter.format(balance) : "******" }}
             </v-text>
-            <v-btn color="button" variant="flat" icon="mdi-eye-outline" size="x-small"></v-btn>
+            <v-btn color="button" variant="flat" size="x-small" @click="isBalanceShown = !isBalanceShown" icon>
+                <v-icon>{{ isBalanceShown ? "mdi-eye-off-outline" : "mdi-eye-outline" }}</v-icon>
+            </v-btn>
         </div>
-        <div class="d-flex justify-space-between mt-3">
+        <div class="d-flex py-2 mt-2 ga-3">
             <v-btn class="v-btn__content text-none" 
                 prepend-icon="mdi-tray-arrow-up"
                 color="button" rounded="xl" variant="flat">Transferir</v-btn>
@@ -21,23 +23,18 @@
     </v-card>
 </template>
 
-<script setup>
-import { computed } from 'vue';
+<script>
+// import { computed } from 'vue';
+    export default{
+        data() {
+            return {
+                isBalanceShown: false,
+                formatter: new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}),
+                balance: 1234567890123
+            };
+        },
+    };
 
-const isBalanceShown = computed(false);
-
-const balance = 123000;
-const balanceString = "$" + balance; 
-
-const balanceIcon = computed(() => 
-    isBalanceShown ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
-)
-// function toggleBalance() {
-//     isBalanceShown ?? 
-// }
-
-// 
-//
 </script>
 
 <style scoped>
@@ -46,6 +43,13 @@ const balanceIcon = computed(() =>
         align-items: center;
     }
     .v-btn__content {
-        letter-spacing: 0.2px;
+        letter-spacing: 0px;
+        font-size: small;
+    }
+    .balance{
+        font-size: x-large; 
+        font-weight: bold;
+        width: 80%; 
+        overflow: hidden;
     }
 </style>
