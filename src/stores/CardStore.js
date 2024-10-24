a6695cc2afa11d0117c25bd537e3a28a5a54d035
Matías Leporini <matilepo03@gmail.com>
2024-10-24 20:31:32 -0300

    Small change

--- src/stores/CardStore.js
+++ src/stores/CardStore.js
@@ -5,28 +5,31 @@ import walletApi from '@/api/wallet'
 export const useCardStore = defineStore('card', () => {
     const previousCards = [
         {
-            id: 0,
-            card_type:'Debito',
-            card_number:'1234 4567 7890 1111',
-            card_owner:'Camila Lee',
-            card_expiry_date:'04/30',
-            card_cvv:'123'
-        },
-        {
             id: 1,
-            card_type:'Debito',
-            card_number:'1234 4567 7890 2222',
-            card_owner:'Camila Lee',
-            card_expiry_date:'04/30',
-            card_cvv:'123'
+            card_brand: 'Visa',
+            card_type: 'Debito',
+            card_number: '1234 4567 7890 0000',
+            card_owner: 'Camila Lee',
+            card_expiry_date: '04/30',
+            card_cvv: '123'
         },
         {
             id: 2,
-            card_type:'Debito',
-            card_number:'1234 4567 7890 3333',
-            card_owner:'Camila Lee',
-            card_expiry_date:'04/30',
-            card_cvv:'123'
+            card_brand: 'Mastercard',
+            card_type: 'Credito',
+            card_number: '9876 5432 1098 7654',
+            card_owner: 'John Smith',
+            card_expiry_date: '12/25',
+            card_cvv: '456'
+        },
+        {
+            id: 3,
+            card_brand: 'American Express',
+            card_type: 'Credito',
+            card_number: '3456 7890 1234 5678',
+            card_owner: 'Emma Watson',
+            card_expiry_date: '09/27',
+            card_cvv: '789'
         }
     ]
     const cards = ref([...previousCards])
